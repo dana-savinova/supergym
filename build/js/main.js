c6314c688da5fe470a4411a39eaa85f28920ece8
dana-savinova <savinovadana@ya.ru>
2020-05-31 15:50:28 +0300

    Добавляет маску для телефона

--- build/js/main.js
+++ build/js/main.js
@@ -25,6 +25,21 @@
 'use strict';
 
 (function () {
+  var userPhoneInput = document.querySelector('input[type="tel"]');
+
+  if (userPhoneInput) {
+    var maskOptions = {
+      mask: '+{7}(000)000-00-00'
+    };
+
+    window.mask = new window.IMask(userPhoneInput, maskOptions);
+  }
+
+})();
+
+'use strict';
+
+(function () {
 
   var sliderReviewsBlock = document.querySelector('.reviews__slider');
 
